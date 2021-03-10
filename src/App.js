2cdb0349a433e4c9ca8c0ec0aeb65ab91d56e000
parth9898 <chauhanparth159@gmail.com>
2021-03-10 14:30:39 -0600

    check app.js file

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
 import React, { Component } from 'react'
+import DatePicker from "react-datepicker";
 import { Button, Form, Container, Header } from 'semantic-ui-react'
 import './App.css';
 
 export default class App extends Component {
-  render() {
+  constructor(props){
+    super(props);
+    this.state = {value:''}
+  
+  this.handleSubmit= this.handleSubmit.bind();
+  }
+  handleSubmit(event)
+  {
+    console.log(document.getElementById("date").value);
+ var date1 = document.getElementById("date").value;
+document.getElementById( 
+              "getdate").innerHTML = date1; 
+              
+      var date3 = document.getElementById("date2").value;        
+              document.getElementById( 
+              "getdate1").innerHTML = date3; 
+  }
+  render() 
+  {
     return (
       <Container fluid className="container">
-        <Header as='h2'>React Google Sheets!</Header>
-        <Form className="form">
+        <Header as='h2'>Date & Time Sheet</Header>
+        <Form className="form" >
           <Form.Field>
-            <label>Name</label>
-            <input placeholder='Enter your name' />
+            <label>IN Date</label>
+            
+  
+            <input type="date" name="date" id="date" onChange={this.handleSubmit} />
+            
           </Form.Field>
           <Form.Field>
-            <label>Age</label>
-            <input placeholder='Enter your age' />
+            <label>OUT Date</label>
+        
+            <input type="date" name="date" id="date2"  onChange={this.handleSubmit}/>
+            
           </Form.Field>
-          <Form.Field>
-            <label>Salary</label>
-            <input placeholder='Enter your salary' />
-          </Form.Field>
-          <Form.Field>
-            <label>Hobby</label>
-            <input placeholder='Enter your hobby' />
-          </Form.Field>
+         
           
           <Button color="blue" type='submit'>Submit</Button>
+
         </Form>
+        <label id="getdate"></label>
+        
+         <label id="getdate1"></label>
       </Container>
     )
   }
